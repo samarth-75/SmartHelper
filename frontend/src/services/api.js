067f813f8dd2d45454d1b29119b1648c7b1dfc46
samarth <samarthmore887@gmail.com>
2026-01-11 00:16:39 +0530

    ....

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://zara-unslating-angla.ngrok-free.dev" });
+const API = axios.create({ baseURL: "https://smarthelper.onrender.com" });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
